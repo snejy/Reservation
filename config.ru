870f843ea3764f650044ba18451c668246817580
snejy <snspasova@gmail.com>
2015-02-13 14:52:00 +0200

    add user model again

--- config.ru
+++ config.ru
@@ -1,10 +1,3 @@
 require './my_application'
 
-Thread.new do
-  while true do
-    sleep 0.12
-    session['time'] = "1"
-  end
-end
-
 PATHS.each { |path, controller| map(path) { run controller } }
---
+++ models/user.rb
@@ -0,0 +1,24 @@
+module Reservation
+  class User
+    include DataMapper::Resource
+
+    property :id, Serial
+
+    property :username, String,
+      :required => true,
+      :length => 6..15,
+      :unique => true,
+      :messages => {
+        :length => "Your username should be between 6 and 15 symbols.",
+        :unique => "This username is already taken."
+      }
+
+    property :password, String,
+      :required => true
+
+    has n, :orders, 'Order',
+      parent_key: [:id],
+      child_key: [:id_user]
+
+  end
+end
